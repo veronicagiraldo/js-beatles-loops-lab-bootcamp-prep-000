1cfae2eb7e532b240681af4dbb8c64deb01ebb24
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 20:53:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments){
-  const=[];
-  for (let i = 0, "1" = musicians.length; i < 1; i++) {
+  const arr=[];
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
 arr.push(`${musicians[i]} plays ${instruments[i]}`)
 }
 return Array
